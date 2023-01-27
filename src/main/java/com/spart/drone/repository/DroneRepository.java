61cab15ff6360988bf68363798028f8d5748a7f9
Spartak <spartak.smirnov@nordclan.com>
2023-01-27 09:36:35 +0300

    1) Refactor DroneEntity

--- src/main/java/com/spart/drone/repository/DroneRepository.java
+++ src/main/java/com/spart/drone/repository/DroneRepository.java
@@ -1,8 +1,9 @@
 package com.spart.drone.repository;
 
+import com.spart.drone.repository.model.DroneEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DroneRepository extends JpaRepository<DroneRepository, Long> {
+public interface DroneRepository extends JpaRepository<DroneEntity, Long> {
 }
