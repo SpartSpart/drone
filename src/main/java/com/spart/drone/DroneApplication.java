68cc3ab157b905de53e399d67e51b7ba4cf6b53f
Spartak <spartak.smirnov@nordclan.com>
2023-01-29 08:02:15 +0300

    CD works fine by starting start.sh

--- src/main/java/com/spart/drone/DroneApplication.java
+++ src/main/java/com/spart/drone/DroneApplication.java
@@ -10,13 +10,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class DroneApplication {
 
 	public static void main(String[] args) {
-//		Flyway flyway = Flyway
-//				.configure()
-//				.dataSource("jdbc:postgresql://localhost:5435/postgres", "drone", "drone")
-//				.load();
-//
-////		 Start the migration
-//		flyway.migrate();
 		SpringApplication.run(DroneApplication.class, args);
 	}
 
